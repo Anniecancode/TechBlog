899eb7d4e701e42ccbd2e7e94fa78a59262a57ee
Anniecancode <99180884+Anniecancode@users.noreply.github.com>
2022-09-25 21:58:26 +0930

    created dashboard handlebar and its route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {User, Post, Comment} = require('../model');
+
 // Import the custom middleware
 const withAuth = require('../utils/auth');
 
@@ -16,12 +17,33 @@ router.get('/',async (req, res, next) => {
     }
 });
 
+// GET user's posts from dashboard page
+router.get('/dashboard/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { user_id } = req.session;
+
+        const data = await Post.findByPk(id, {
+            include: [{
+                model: Comment,
+                include: [{ model: User }],
+                model: User
+            }],
+        });
+        const posts = data.map(post => post.get({ plain: true }));
+        res.render('dashboard', { 
+            ...posts,
+        edit_mode: this.posts.user_id === user_id });
+    } catch(error) {
+        next(error)
+    }
+});
+
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
         return;
     }
-
     res.render('login')
 });
 
