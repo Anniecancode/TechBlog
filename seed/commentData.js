1a76a1b5b44e4bcc7bf4f0b6ca3a3ec151056309
Anniecancode <99180884+Anniecancode@users.noreply.github.com>
2022-09-25 16:01:28 +0930

    push to github

--- seed/commentData.js
+++ seed/commentData.js
@@ -5,15 +5,15 @@ const commentdata = [
        comment_title: '',
        comment_content: '',
        date: '24/09/2022',
-       user_id: '',
-       post_id: ''
+       user_id: '1',
+       post_id: '1'
     },
     {
         comment_title: '',
         comment_content: '',
         date: '24/09/2022',
-        user_id: '',
-        post_id: ''
+        user_id: '1',
+        post_id: '2'
      },
 ];
 
